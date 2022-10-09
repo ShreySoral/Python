3b317bd795c333877bf73dbc09df24493dcaf86e
Shrey Soral <78461926+ShreySoral@users.noreply.github.com>
2022-10-09 03:19:18 -0400

    Add files via upload

---
+++ lists_in_python.py
@@ -0,0 +1,19 @@
+grocery=["harpic","vim bar","deodrant","bhindi","lollypop",56]
+print(grocery)
+print(grocery[5])
+numbers=[2,7,9,11,3]
+print(numbers)
+# numbers.sort()
+# numbers.reverse()
+# print(numbers)
+print(numbers[:])
+print(numbers[1:])
+print(numbers[1:4])
+print(numbers[::2])
+print(numbers[1::-3])
+print(numbers[1:5:2])
+numbers.append(7)
+print(numbers)
+li=[]
+li.append(numbers)
+print(li)
