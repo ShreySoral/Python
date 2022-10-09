6dc1e185efafe03ecf58f3d092b75976554961d1
Shrey Soral <78461926+ShreySoral@users.noreply.github.com>
2022-10-09 02:36:19 -0400

    Add files via upload

---
+++ strings_in_python.py
@@ -0,0 +1,16 @@
+mystr="Shrey is a good boy"
+print(mystr)
+print(len(mystr))
+print(mystr[0:5:2])
+print(mystr[0:19])
+print(mystr[0:19:1])
+print(mystr[::9])
+print(mystr[15:17]) 
+print(mystr[::-1]) #Reverse the string
+print(mystr[::-2])
+
+print(mystr.isalnum())
+print(mystr.isalpha())
+print(mystr.count("o"))
+print(mystr.endswith("boy"))
+print(mystr.capitalize())
